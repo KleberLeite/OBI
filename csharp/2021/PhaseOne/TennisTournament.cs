8d0395a884272fce1c657d341af05a1715b394da
kleberleite <kleberleitetavares@gmail.com>
2022-08-05 23:04:54 -0300

    feat: 2021/PhaseOne TennisTournament

---
+++ csharp/2021/PhaseOne/TennisTournament.cs
@@ -0,0 +1,13 @@
+
+public static class TennisTournament
+{
+    public static void Resolve()
+    {
+        var wins = 0;
+        for (int i = 0; i < 6; i++)
+            if (Console.ReadLine() == "V")
+                wins++;
+
+        Console.WriteLine(wins >= 5 ? 1 : wins >= 3 ? 2 : wins >= 1 ? 3 : -1);
+    }
+}
