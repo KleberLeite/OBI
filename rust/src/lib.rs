e87b15e800d987d0c794f2ce89b48351aa3e493c
Arthur Damasceno <damascenoarthur653@gmail.com>
2022-08-06 12:05:46 +0000

    setup rust project

---
+++ rust/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn read() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_end().into()
+}
+
+pub fn read_u8() -> u8 {
+    read().parse().unwrap()
+}
+
+pub fn read_char() -> char {
+    read().remove(0)
+}
